8cb39a5c440b0e0355c702e132ea15b37e4048f3
Yu.A.Maykov@yandex.ru <Yu.A.Maykov@yandex.ru>
2019-11-01 09:32:31 -0700

    commit

--- Vk_Maykov/Source/BLL/DataManager.swift
+++ Vk_Maykov/Source/BLL/DataManager.swift
@@ -6,4 +6,60 @@
 //  Copyright © 2019 Yuriy. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DataManager {
+    //MARK: - формирование данных списка друзей
+    var friends:[FriendModel] = []
+    
+    func addItem(friendItem: FriendModel) {
+        friends.append(friendItem)
+    }
+    
+    func addItems(friendArray: [FriendModel]) {
+        friendArray.forEach { item in
+            friends.append(item)
+        }
+    }
+    
+    func removeItem(indx: Int) {
+        friends.remove(at: indx)
+    }
+    
+    func clearAll() {
+        friends.removeAll()
+    }
+
+    //getVkAPI.getFriends { qResult in print(qResult) }
+
+    func friendsConvertData() {
+        let getAPI = GetVKAPI()
+        getAPI.getFriends { qResult in
+            guard let items:[ParsModels.items] = qResult.response.items else {
+                print("Ошибка. Нет массива с данными!")
+                return
+            }
+            
+            if items.count > 0 {
+                for item in items {
+                    print(item.id)
+                    print(item.lastName ?? " -- ")
+                    print(item.firstName ?? " -- ")
+                    print(item.photo50 ?? " -- ")
+
+                    self.addItem(friendItem: FriendModel(
+                        userId: item.id,
+                        userName: (item.lastName ?? "") + " " + (item.firstName ?? ""),
+                        userAvatar: UIImage.empty,
+                        userEmail: ""
+                        )
+                    )
+                    print(self.friends[0].userName)
+                }
+                
+            }
+            
+        }
+    }
+    
+}
